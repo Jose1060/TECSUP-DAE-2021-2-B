f00e3c4570ee91db9131252823aac45f63d0839f
Jose1060 <84330500+Jose1060@users.noreply.github.com>
2021-08-23 18:42:18 -0500

    lab1

--- lab01/app/urls.py
+++ lab01/app/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('sumar/<int:num1>/<int:num2>/', views.sumar, name='sumar'),
     # ex: localhost:8080/app/restar/18/19
     path('restar/<int:num1>/<int:num2>/', views.restar, name='restar'),
-    # ex: localhost:8080/app/sumar/18/19
+    # ex: localhost:8080/app/multiplicar/18/19
     path('multiplicar/<int:num1>/<int:num2>/', views.multiplicar, name='multiplicar'),
-    # ex: localhost:8080/app/sumar/18/19
+    # ex: localhost:8080/app/dividir/18/19
     path('dividir/<int:num1>/<int:num2>/', views.dividir, name='dividir'),
 
 
