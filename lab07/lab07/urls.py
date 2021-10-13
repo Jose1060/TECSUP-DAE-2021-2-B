851bc3d450c80b49411d78c8eaab2682063469fe
Jose1060 <84330500+Jose1060@users.noreply.github.com>
2021-10-12 23:23:02 -0500

    lab07

--- lab07/lab07/urls.py
+++ lab07/lab07/urls.py
@@ -1,4 +1,4 @@
-"""lab06 URL Configuration
+"""lab07 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('tienda/', include('tienda.urls')),
     path('admin/', admin.site.urls),
-    path('tienda/', include('tienda.urls'))
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- lab07/tienda/admin.py
+++ lab07/tienda/admin.py
@@ -4,6 +4,8 @@ from .models import Producto
 
 # Register your models here.
 
-
 admin.site.register(Categoria)
 admin.site.register(Producto)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('categoria', 'nombre', 'precio', 'stock')
