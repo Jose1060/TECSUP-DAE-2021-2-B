10cc5e699c049d468063e1b5bb2d2f1250179579
Jose1060 <84330500+Jose1060@users.noreply.github.com>
2021-11-14 22:07:15 -0500

    practica3

---
+++ Biblioteca/backend/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Prestamos
+
+
+class PrestamoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prestamos
+        fields = '__all__'
