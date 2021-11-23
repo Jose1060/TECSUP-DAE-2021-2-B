4252f7bd18b50b12f739545c1acea6ecf4e3bc1f
Jose1060 <84330500+Jose1060@users.noreply.github.com>
2021-11-22 22:38:29 -0500

    lab13

--- lab13/src/index.js
+++ lab13/src/index.js
@@ -1,3 +1,4 @@
+import logo from "./logo.svg";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -7,21 +8,39 @@ import { Route, Link, BrowserRouter, Routes } from "react-router-dom";
 import Users from "./users";
 import Contact from "./contact";
 import Notfound from "./notfound";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const routing = (
 	<div>
 		<BrowserRouter>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/usuarios">Users</Link>
-				</li>
-				<li>
-					<Link to="/contacto">Contact</Link>
-				</li>
-			</ul>
+			<Navbar
+				bg="dark"
+				variant="dark"
+				sticky="top"
+				expand="sm"
+				collapseOnSelect>
+				<Navbar.Brand>
+					<img src={logo} width="40px" height="40px" /> Logo
+				</Navbar.Brand>
+
+				<Navbar.Toggle className="coloring" />
+				<Navbar.Collapse>
+					<Nav>
+						<NavDropdown title="Usuarios">
+							<NavDropdown.Item href="/usuarios/1">Usuario 1</NavDropdown.Item>
+							<NavDropdown.Item href="/usuarios/2">Usuario 2</NavDropdown.Item>
+							<NavDropdown.Item href="/usuarios/3">Usuario 3</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="/usuarios">Todos</NavDropdown.Item>
+						</NavDropdown>
+						<Nav.Link href="/contacto">Contacto</Nav.Link>
+						<Nav.Link href="/*">About Us</Nav.Link>
+						<Nav.Link href="/*">Contact Us</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+
 			<Routes>
 				<Route exact path="/" element={<App />} />
 				<Route path="/usuarios/:id" element={<Users />} />
